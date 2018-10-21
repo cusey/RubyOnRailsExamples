23337e2623a3293383311bdb20286fe89abee764
joe99peace <jjcusey@gmail.com>
2018-10-21 17:09:53 -0400

    Sending Json back from controller

--- simpleblog/app/controllers/posts_controller.rb
+++ simpleblog/app/controllers/posts_controller.rb
@@ -5,4 +5,10 @@ class PostsController < ApplicationController
     #localhost:3000/posts/new
     def new
     end
+    def create
+        render plain:params[:post].inspect
+        #After clicking the submit button on the 'localhost:3000/posts/new' page
+        # New page URL: 'http://localhost:3000/posts'
+        #<ActionController::Parameters {"title"=>"adfasdf", "body"=>"asdffasdf"} permitted: false>
+    end
 end

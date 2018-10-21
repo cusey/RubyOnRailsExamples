eeb74f214a5b1eec3bbfd576fb3ec9ca4b2acdb7
joe99peace <jjcusey@gmail.com>
2018-10-21 15:45:20 -0400

    about page

--- simpleblog/config/routes.rb
+++ simpleblog/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'posts#index'
+
+#Normal Rout
+# get <localhost:3000/about> => <controller-name#veiw-name>
+get 'about'=> 'pages#about'
 end

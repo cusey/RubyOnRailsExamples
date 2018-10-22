871282a8530fe156e983513fe78e04fe832071e3
joe99peace <jjcusey@gmail.com>
2018-10-22 18:20:12 -0400

    Checking Form
    Checking the  form by sending sending JSON to the browser

--- simpleblog/app/controllers/posts_controller.rb
+++ simpleblog/app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
     def new
     end
     def create
-        render plain:params[:post].inspect
+        #render plain:params[:post].inspect
         #After clicking the submit button on the 'localhost:3000/posts/new' page
         # New page URL: 'http://localhost:3000/posts'
         #<ActionController::Parameters {"title"=>"adfasdf", "body"=>"asdffasdf"} permitted: false>

dd0a14f89b22b87864f1108a8c2d80a092bbec69
joe99peace <jjcusey@gmail.com>
2018-10-27 22:24:26 -0400

    Update simpleblog/app/controllers/pages_controller.rb

--- simpleblog/app/controllers/pages_controller.rb
+++ simpleblog/app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
     #http://localhost:3000/about
     def about
         @title='About Us'
-        @content='This is the about page'
+        @content='This is Rails on Rails application people can uses to build their our Blog Webpage.'
     end
 end
